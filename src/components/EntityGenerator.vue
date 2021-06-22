56ae5f9476bf03fd2412303863fd49b897178b43
BruceKangCN <brucekangcn@gmail.com>
2021-06-22 17:42:23 +0800

    refactor code block

--- src/components/EntityGenerator.vue
+++ src/components/EntityGenerator.vue
@@ -1,31 +1,27 @@
 <template>
   <!-- 文件名 -->
   <h3><code>{{ pascal }}.java</code></h3>
-<!-- 标签前不添加缩进，否则代码块内容会出现多余缩进 -->
-<!-- v-if判断是否渲染引入UUID的代码需要与上一行代码写在同一行，并在行首添加换行符，以保证Java代码格式正常 -->
-<pre v-highlightjs id="entity-java">
-<code class="java">package {{ basePackage }}.entity;</code>
-<code class="java"></code>
-<code class="java">import lombok.AllArgsConstructor;</code>
-<code class="java">import lombok.Data;</code>
-<code class="java">import lombok.NoArgsConstructor;</code>
-<code class="java"></code>
-<code class="java">import javax.persistence.*;</code>
-<code class="java">import java.io.Serializable;</code><code class="java" v-if="entity.id.type === 'UUID'">
-<!-- id = UUID? -->import java.util.UUID;</code>
-<code class="java"></code>
-<code class="java">@Entity</code>
-<code class="java">@Table(name = "{{ snake }}")</code>
-<code class="java">@Data</code>
-<code class="java">@AllArgsConstructor</code>
-<code class="java">@NoArgsConstructor</code>
-<code class="java">public class {{ pascal }} implements Serializable {</code>
-<code class="java">    @Id</code>
-<code class="java">    @GeneratedValue</code>
-<code class="java">    private {{ entity.id.type }} {{ entity.id.name }};</code>
-<code class="java" v-for="field in entity.fields" :key="field"><br/>    private {{ field.type }} {{ field.name }};</code>
-<code class="java">}</code>
-</pre>
+  <!-- &#10;为<LF>换行符，需添加到下一行的行首，否则不显示 -->
+  <div id="entity-java">
+    <pre v-highlightjs><code class="java">package {{ basePackage }}.entity;</code></pre>
+    <pre v-highlightjs><code class="java">&#10;import lombok.AllArgsConstructor;</code></pre>
+    <pre v-highlightjs><code class="java">import lombok.Data;</code></pre>
+    <pre v-highlightjs><code class="java">import lombok.NoArgsConstructor;</code></pre>
+    <pre v-highlightjs><code class="java">&#10;import javax.persistence.*;</code></pre>
+    <pre v-highlightjs><code class="java">import java.io.Serializable;</code></pre>
+    <pre v-highlightjs><code class="java" v-if="entity.id.type === 'UUID'">import java.util.UUID;</code></pre>
+    <pre v-highlightjs><code class="java">&#10;@Entity</code></pre>
+    <pre v-highlightjs><code class="java">@Table(name = "{{ snake }}")</code></pre>
+    <pre v-highlightjs><code class="java">@Data</code></pre>
+    <pre v-highlightjs><code class="java">@AllArgsConstructor</code></pre>
+    <pre v-highlightjs><code class="java">@NoArgsConstructor</code></pre>
+    <pre v-highlightjs><code class="java">public class {{ pascal }} implements Serializable {</code></pre>
+    <pre v-highlightjs><code class="java">    @Id</code></pre>
+    <pre v-highlightjs><code class="java">    @GeneratedValue</code></pre>
+    <pre v-highlightjs><code class="java">    private {{ entity.id.type }} {{ entity.id.name }};</code></pre>
+    <pre v-highlightjs><code class="java" v-for="field in entity.fields" :key="field"><br/>    private {{ field.type }} {{ field.name }};</code></pre>
+    <pre v-highlightjs><code class="java">}&#10;</code></pre>
+  </div>
 </template>
 
 <script>
@@ -42,11 +38,14 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-pre {
+#entity-java {
   background-color: #0d1117;
   width: 80ch;
-  font-family: 'Cascadia Mono PL', monospace;
   max-height: 30ch;
   overflow: auto;
+}
+pre {
+  margin: 0px;
+  font-family: 'Cascadia Mono PL', monospace;
 }
 </style>
